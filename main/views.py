096af3ff8749c00f0a88ad57a169733c480e89b1
Juan Arturo <cripto1989@gmail.com>
2018-11-10 14:40:57 -0600

    It adds 2 methos for save data

--- main/views.py
+++ main/views.py
@@ -1,22 +1,32 @@
 import json
 
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from colorama import Fore, init
+
 init()
+
 
 class test(APIView):
 
     def post(self, request, format=None):
         # print(json.dumps(self.request.data))
         print(Fore.GREEN + json.dumps(self.request.data))
-        data = {    
+        data = {
         }
-        return Response(data=data,status=status.HTTP_200_OK)
+        return Response(data=data, status=status.HTTP_200_OK)
 
+    def validate_data(self):
+        """
+        Here we have to validate if the data is in request.data
+        :return:
+        """
+        pass
 
-
-
-    
+    def save_json(self):
+        """
+        Here we need to save the history
+        :return:
+        """
+        pass
