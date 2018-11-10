efd3163835a37b6db7da0b48c9ab29d541319b28
Marco Antonio Rodriguez Torres <mrc5622@gmail.com>
2018-11-10 14:43:18 -0600

    this add model DataModel

--- main/models.py
+++ main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from model_utils.models import TimeStampedModel
+from django.utils.translation import gettext as _
+from model_utils import Choices
 
 # Create your models here.
+
+
+class DataUser(TimeStampedModel):
+    session_id = models.CharField(max_length=400)
+    user_name = models.CharField(max_length=50)
+    jobcoach_name = models.CharField(max_length=50)
+    user_email = models.EmailField()
+    GENDER = Choices((1, 'male', _('Masculino')), (2, 'female', _('Femenino')))
+    gender = models.PositiveSmallIntegerField(choices=GENDER, default=GENDER.male)
+    EMOTION = Choices((1, 'emotion_frustrated', _('Frustado')), (2, 'emotion_sad', _('Triste')),(1, 'emotion_irritated', _('Irritado')))
+    emotion = models.PositiveSmallIntegerField(choices=EMOTION, default=EMOTION.emotion_frustrated)
+
+    def __str__(self):
+        return "{}".format(self.session_id)
