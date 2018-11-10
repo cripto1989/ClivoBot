0a3ae002e325e7cc73f756afdd9242c4932ad8bc
Juan Arturo <cripto1989@gmail.com>
2018-11-10 14:28:05 -0600

    It adds colorama

--- main/views.py
+++ main/views.py
@@ -4,13 +4,15 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from colorama import Fore, init
+init()
 
 class test(APIView):
 
     def post(self, request, format=None):
-        print(json.dumps(self.request.data))
-        data = {
-            "fulfillmentText": "Hola Fulano"
+        # print(json.dumps(self.request.data))
+        print(Fore.GREEN + json.dumps(self.request.data))
+        data = {    
         }
         return Response(data=data,status=status.HTTP_200_OK)
 
