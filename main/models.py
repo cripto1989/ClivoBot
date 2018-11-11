cf0790f619869d9207053cb3bd195208b557a361
Marco Antonio Rodriguez Torres <mrc5622@gmail.com>
2018-11-11 14:00:34 -0600

    Mdified DataUser Model

--- main/models.py
+++ main/models.py
@@ -13,11 +13,10 @@ class DataUser(TimeStampedModel):
     jobcoach_name = models.CharField(max_length=50, blank=True, null=True)
     email = models.EmailField(blank=True, null=True)
     GENDER = Choices((1, 'male', _('Masculino')), (2, 'female', _('Femenino')))
-    user_gender = models.PositiveSmallIntegerField(choices=GENDER, default=GENDER.male, blank=True, null=True)
+    user_gender = models.PositiveSmallIntegerField(choices=GENDER, blank=True, null=True)
     EMOTION = Choices((1, 'emotion_frustrated', _('Frustado')),
                       (2, 'emotion_sad', _('Triste')), (3, 'emotion_irritated', _('Irritado')))
-    emotion_neg = models.PositiveSmallIntegerField(choices=EMOTION, default=EMOTION.emotion_frustrated,
-                                               blank=True, null=True)
+    emotion_neg = models.PositiveSmallIntegerField(choices=EMOTION, blank=True, null=True)
 
     def __str__(self):
         return "{}".format(self.session_id)
