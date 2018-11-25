900bba3e4e0382b70c6cb96feb2d4acee74bdea7
Diego Herrera <jdht1992@gmail.com>
2018-11-25 15:34:42 -0600

    created custom firebase class

--- main/firebase.py
+++ main/firebase.py
@@ -1,14 +1,20 @@
 import firebase_admin 
 from firebase_admin import credentials
 from firebase_admin import db
-from django.conf.settings import FIREBASE_APP_CLIVO
+from django.conf import settings
 
 cred = credentials.Certificate('credentials.json')
 firebase_admin.initialize_app(cred, {
-   'databaseURL': FIREBASE_APP_CLIVO
+   'databaseURL': settings.FIREBASE_APP_CLIVO
 })
 
-ref = db.reference('Users')
-data = ref.get()
-print(json.loads(data))
+class CustomFirebase:
+
+   @classmethod
+   def get_users(cls):
+      ref = db.reference('Users')
+      data = ref.get()
+      return data
+
+
 
