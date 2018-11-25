820e2b31e46e297c9aef21aa61561bdf2afaced2
Arturo Herrera <cripto1989@gmail.com>
2018-11-25 15:18:19 -0600

    It creates CustomFirebase class

--- main/firebase.py
+++ main/firebase.py
@@ -9,12 +9,32 @@ firebase_admin.initialize_app(cred, {
 })
 
 class CustomFirebase:
+   """
+   This class let us get information from FireBase
+   """
 
    @classmethod
    def get_users(cls):
+      """
+      Get all the users saved in Firebase.
+      :return dict Data of all users saved in Firebase
+      """
       ref = db.reference('Users')
       data = ref.get()
       return data
+
+   @classmethod
+   def get_coach_email(cls, coach_name):
+      """      
+      Get the email coach in firebase data.
+      :param coach_name: Coach name e.g Linux Torvalds
+      """
+      data = cls.get_users()
+      for value in data.values():         
+         if 'name' in value:
+            if coach_name.lower() == value['name'].lower():                           
+               return value['email']            
+      
 
 
 
